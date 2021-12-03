73026cee27de3c4a1bd99bf0bd467b84a39fabed
name52 <it.cucum@gmail.com>
2021-12-03 16:20:47 +0900

    Init 깃 이전작업들

---
+++ script.js
@@ -0,0 +1,9 @@
+var mixOption = {
+    selectors: { // default: .mix
+        target: '.mix' 
+    },
+    animation: {
+        duration: 300
+    }
+}
+var mixer = mixitup('.container', mixOption);
